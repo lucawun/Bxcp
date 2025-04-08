78a00fb6d65f4a34ddc5d9364be5a613bb9d62f1
Luca Wunderlich <mail@luca-wunderlich.de>
2025-04-08 22:08:22 +0200

    Add CountryMapper and WeatherMapper

---
+++ Bxcp.Application/Mappers/CountryMapper.cs
@@ -0,0 +1,24 @@
+﻿using Bxcp.Application.DTOs;
+using Bxcp.Domain.Models;
+
+namespace Bxcp.Application.Mappers;
+
+/// <summary>
+/// Maps between domain models and DTOs for country data
+/// </summary>
+public static class CountryMapper
+{
+    /// <summary>
+    /// Maps a domain model to a DTO
+    /// </summary>
+    /// <param name="countryRecord">The domain model to map</param>
+    /// <returns>A DTO representing the country analysis result</returns>
+    public static CountryAnalysisResult ToCountryAnalysisResult(CountryRecord countryRecord)
+    {
+        return new CountryAnalysisResult
+        {
+            CountryWithHighestDensity = countryRecord.Name,
+            HighestDensity = countryRecord.PopulationDensity
+        };
+    }
+}
