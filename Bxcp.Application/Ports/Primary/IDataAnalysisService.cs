4e130cdf0dedbc974519507c18ad85051504507e
Luca Wunderlich <mail@luca-wunderlich.de>
2025-04-08 22:07:57 +0200

    Add data analysis and file reading interfaces

---
+++ Bxcp.Application/Ports/Primary/IDataAnalysisService.cs
@@ -0,0 +1,24 @@
+﻿using Bxcp.Application.DTOs;
+
+namespace Bxcp.Application.Ports.Primary;
+
+/// <summary>
+/// Primary port for data analysis operations
+/// This interface defines the entry points to the application core
+/// </summary>
+public interface IDataAnalysisService
+{
+    /// <summary>
+    /// Analyzes weather data to find the day with the smallest temperature spread
+    /// </summary>
+    /// <param name="filePath">Path to the weather data file</param>
+    /// <returns>Analysis result containing the day with smallest temperature spread</returns>
+    WeatherAnalysisResult AnalyzeWeatherData(string filePath);
+
+    /// <summary>
+    /// Analyzes country data to find the country with the highest population density
+    /// </summary>
+    /// <param name="filePath">Path to the country data file</param>
+    /// <returns>Analysis result containing the country with highest population density</returns>
+    CountryAnalysisResult AnalyzeCountryData(string filePath);
+}
---
+++ Bxcp.Application/Ports/Secondary/IFileReader.cs
@@ -0,0 +1,16 @@
+﻿namespace Bxcp.Application.Ports.Secondary;
+
+/// <summary>
+/// Secondary port for reading data from files
+/// This interface will be implemented by adapters in the infrastructure layer
+/// </summary>
+/// <typeparam name="T">The type of data to be read from the file</typeparam>
+public interface IFileReader<T>
+{
+    /// <summary>
+    /// Reads all records from a file
+    /// </summary>
+    /// <param name="filePath">Path to the file to read</param>
+    /// <returns>An enumerable collection of records</returns>
+    IEnumerable<T> ReadAllRecords(string filePath);
+}
