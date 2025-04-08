0a228e438c085f07b70ff0ad026976322f69536d
Luca Wunderlich <mail@luca-wunderlich.de>
2025-04-08 22:28:24 +0200

    Add InfrastructureConfiguration for DI services

---
+++ Bxcp.Infrastructure/Configuration/InfrastructureConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Bxcp.Application.Ports.Secondary;
+using Bxcp.Infrastructure.Adapters.FileSystem;
+using Bxcp.Infrastructure.DTOs;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bxcp.Infrastructure.Configuration;
+
+/// <summary>
+/// Configuration class for infrastructure services
+/// </summary>
+public static class InfrastructureConfiguration
+{
+    /// <summary>
+    /// Adds infrastructure services to the DI container
+    /// </summary>
+    /// <param name="services">The service collection</param>
+    /// <returns>The updated service collection</returns>
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+    {
+        // Register file readers
+        services.AddScoped<IFileReader<WeatherCsvRecord>, CsvWeatherFileReader>();
+        services.AddScoped<IFileReader<CountryCsvRecord>, CsvCountryFileReader>();
+
+        return services;
+    }
+}
