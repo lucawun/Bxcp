66252dea2ab4dc573fb21a651334e30475472e04
Luca Wunderlich <mail@luca-wunderlich.de>
2025-04-08 22:07:07 +0200

    Add DTO classes for country and weather analysis results

---
+++ Bxcp.Application/DTOs/CountryAnalysisResult.cs
@@ -0,0 +1,17 @@
+﻿namespace Bxcp.Application.DTOs;
+
+/// <summary>
+/// Data Transfer Object containing the results of a country data analysis
+/// </summary>
+public record CountryAnalysisResult
+{
+    /// <summary>
+    /// The name of the country with the highest population density
+    /// </summary>
+    public string CountryWithHighestDensity { get; init; } = string.Empty;
+
+    /// <summary>
+    /// The value of the highest population density
+    /// </summary>
+    public double HighestDensity { get; init; }
+}
---
+++ Bxcp.Application/DTOs/WeatherAnalysisResult.cs
@@ -0,0 +1,17 @@
+﻿namespace Bxcp.Application.DTOs;
+
+/// <summary>
+/// Data Transfer Object containing the results of a weather data analysis
+/// </summary>
+public record WeatherAnalysisResult
+{
+    /// <summary>
+    /// The day number with the smallest temperature spread
+    /// </summary>
+    public int DayWithSmallestTemperatureSpread { get; init; }
+
+    /// <summary>
+    /// The value of the smallest temperature spread
+    /// </summary>
+    public double SmallestTemperatureSpread { get; init; }
+}
