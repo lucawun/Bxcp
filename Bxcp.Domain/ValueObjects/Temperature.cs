fe292af227d24262fb811579d6cc4338a71e2011
Luca Wunderlich <mail@luca-wunderlich.de>
2025-04-08 20:52:59 +0200

    Add Temperature value object

---
+++ Bxcp.Domain/ValueObjects/Temperature.cs
@@ -0,0 +1,22 @@
+﻿namespace Bxcp.Domain.ValueObjects;
+
+/// <summary>
+/// Represents a temperature value with conversion capabilities.
+/// </summary>
+public record Temperature
+{
+    public double Celsius { get; }
+    private Temperature(double celsius)
+    {
+        Celsius = celsius;
+    }
+        public static Temperature FromCelsius(double celsius) => new(celsius);
+
+    public static Temperature FromFahrenheit(double fahrenheit) => new((fahrenheit - 32) * 5 / 9);
+
+    public double ToFahrenheit() => Celsius * 9 / 5 + 32;
+
+    public static implicit operator double(Temperature temperature) => temperature.Celsius;
+
+    public override string ToString() => $"{Celsius:F1}°C";
+}
