feb39f1cd3a171a664f47abe87ae66ae23e64244
Luca Wunderlich <mail@luca-wunderlich.de>
2025-04-08 22:09:06 +0200

    Add Data Analysies Services

---
+++ Bxcp.Application/Exceptions/AnalysisFailedException.cs
@@ -0,0 +1,12 @@
+﻿namespace Bxcp.Domain.Exceptions;
+
+/// <summary>
+/// Exception thrown when data analysis fails
+/// </summary>
+public class AnalysisFailedException : DomainException
+{
+    public AnalysisFailedException(string message) : base(message) { }
+
+    public AnalysisFailedException(string message, Exception innerException)
+        : base(message, innerException) { }
+}
---
+++ Bxcp.Application/Exceptions/EmptyDataException.cs
@@ -0,0 +1,12 @@
+﻿namespace Bxcp.Domain.Exceptions;
+
+/// <summary>
+/// Exception thrown when no data is available for analysis
+/// </summary>
+public class EmptyDataException : DomainException
+{
+    public EmptyDataException(string message) : base(message) { }
+
+    public EmptyDataException(string message, Exception innerException)
+        : base(message, innerException) { }
+}
